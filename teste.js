3acc4f6c37022cdf0359db5e1dbd962817f7cf91
Ricardo <ric.asjunior@gmail.com>
2023-05-12 12:36:58 -0300

    Funcao Incrementa itens

--- teste.js
+++ teste.js
@@ -4,20 +4,24 @@ const bubbleSort = document.getElementById('bubble_sort')
 
 let itens = [] 
 
-
-
-
-
 const adicionarElemento = document.getElementById('btn-adicionar')
 adicionarElemento.addEventListener('click', (evento) => {
     evento.preventDefault()
     let input = document.querySelector('#elemento')
     itens.push(input.value)
     console.log(itens)
+    adicionandoElementoNaTela(itens)
 })
 
 
-
+function adicionandoElementoNaTela(itens){
+    valores.innerHTML = ''
+    itens.forEach(itens => {
+        valores.innerHTML += `
+        <li>${itens}</li>
+        `
+    })
+}
 
 
 
