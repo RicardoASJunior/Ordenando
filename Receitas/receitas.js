f1153f35c4b0451d9cd079d4bc09594aef6e88bc
Ricardo <ric.asjunior@gmail.com>
2023-05-16 12:40:56 -0300

    Criando a página de receitas, arquivoJSON

---
+++ Receitas/receitas.js
@@ -0,0 +1,15 @@
+function getListaIngredientes(receita){
+    forEach(lista , () => {
+        `<li>${elementoLista}</li>`
+    })
+}
+
+function getCard(receita){
+
+}
+
+function preencheCatalogo(){
+
+}
+
+// onload ?????????
