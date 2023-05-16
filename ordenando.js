25a9f3df1e4c393bce84ddee5c831256e5482bd3
Ricardo <ric.asjunior@gmail.com>
2023-05-16 11:57:21 -0300

    Refatorando

--- ordenando.js
+++ ordenando.js
@@ -1,71 +1,114 @@
-// const form = document.getElementById("novoItem")
-// const valores = document.getElementById("valores")
-// const limparLista = document.getElementById("limpar")
-// const adicionarElemento = document.getElementById("adicionar")
-// const bubbleSort = document.getElementById("bubble_sort")
-// let itens = [] 
+const valores = document.getElementById('valores')
+const limparLista = document.getElementById('btn-limpar')
+const adicionarElemento = document.getElementById('btn-adicionar')
+const ordenar = document.getElementById('btn-ordenar')
+const misturar = document.getElementById('btn-misturar')
+let itens = [] 
 
 
+adicionarElemento.addEventListener('click', (evento) => {
+    evento.preventDefault()
+    let input = document.querySelector('#elemento')
+    itens.push(parseInt(input.value))
+    console.log(itens)
+    adicionandoElementoNaTela(itens)
+})
 
 
+function adicionandoElementoNaTela(lista){
+    valores.innerHTML = ''
+    lista.forEach(lista => {
+        valores.innerHTML += `
+        <li>${lista}</li>
+        `
+    })
+}
 
-// function swap(){
-//     null
-// };
+limparLista.addEventListener('click', (evento) =>{
+    evento.preventDefault()
+    itens = []
+    adicionandoElementoNaTela(itens)
+})
 
-// function shuffle(){
-//     null
-// };
+function atualizarSelect(){
+    let select = document.querySelector("#organiza")
+    let optionValue = select.options[select.selectedIndex]
+    let valorSelect = optionValue.value
 
-// function bubble_sort(itens){
-//     let swap;
-//     let last =  itens.length -1;
-//     do {
-//         swap=false;
-//         for(let i=0; i < last; i++){
-//             if (itens[i] > itens[i+1]){
-//                 [itens[i], itens[i + 1]] = [itens[i + 1], itens[i]];
-//                 swap = true;
-//             }
-//         }
-//         last--;
-//     } while(swap)
-//     return itens;
-// }
- 
-// function selection_sort(vetor){
-//     let menor;
+    return valorSelect
+}
 
-//     for (let i = 0; i < vetor.length - 1; i++){
-//         menor = i;
-//         for(let j = i + 1; j < vetor.length; j++){
-//             if (vetor[j] < vetor [menor]){
-//                 menor = j;
-//             }
-//         }
-//         if (i != menor){
-//             [vetor[i], vetor[menor]] = [vetor[menor], vetor[i]];
-//         }
-//     }
-//     return vetor;
-// }
+ordenar.addEventListener('click', (evento) => {
+    evento.preventDefault()
+    let valorDoSelect = atualizarSelect()  
+    if (valorDoSelect === "sorted"){
+        selection_sort(itens)
+        adicionandoElementoNaTela(itens)
+    }
+    if (valorDoSelect === "quick_sort"){
+        quick_sort(itens)
+        itens = quick_sort(itens)
+        adicionandoElementoNaTela(itens)
+    }
+    if (valorDoSelect === "bubble_sort"){
+        bubble_sort(itens)
+        adicionandoElementoNaTela(itens)
+    }
+})
 
-// function quick_sort(){
-//     if (Array.length === 0) return [];
-//     if (Array.length === 1) return Array;
+function bubble_sort(lista) {
+    var tamanhoLista = lista.length;  
+    for (var i = 0; i < tamanhoLista; i++) {
+          for (var j = 0; j < (tamanhoLista - i - 1); j++) {
+                   if(lista[j] > lista[j+1]) {
+                           var posicaoLista = lista[j]; 
+                lista[j] = lista[j+1]; 
+                lista[j+1] = posicaoLista; 
+            }
+        }        
+    }
+}
 
-//     var pivot = array[0];
 
-//     var head = array.filter(n => n < pivot);
-//     var equal = array.filter(n => n === pivot);
-//     var tail = array.filter(n => n < pivot);
+misturar.addEventListener('click', (evento) => {
+    evento.preventDefault()
+    shuffle(itens)
+    adicionandoElementoNaTela(itens)
+})
 
-//     return quick_sort(head).concat(equal).concat(quick_sort(tail));
+function shuffle(lista) {
+    for (var j, x, i = lista.length; i; j = Math.floor(Math.random() * i), x = lista[--i], lista[i] = lista[j], lista[j] = x);
+    return lista;
+}
 
-// }
+function quick_sort(lista){
+    const pivo = lista[lista.length - 1];
+    const leftLista = [];
+    const rightLista = [];
 
-// function particionamento(){
-//     null
-// };
-// //--------------------------------------------------------------------
-
+    if(lista.length <= 1){
+      return lista;
+    }
+    for(let i=0; i < lista.length-1;i++){
+      lista[i] < pivo ? leftLista.push(lista[i]) :  rightLista.push(lista[i])
+    }
+    return [...quick_sort(leftLista) ,pivo,...quick_sort(rightLista)];
+}
+  
+function selection_sort(lista) { 
+    let tamanhoLista = lista.length;
+    for(let i = 0; i < tamanhoLista; i++) {
+        let posicao = i;
+        for(let j = i+1; j < tamanhoLista; j++){
+            if(lista[j] < lista[posicao]) {
+                posicao=j; 
+            }
+         }
+         if (posicao != i) {
+             let tmp = lista[i]; 
+             lista[i] = lista[posicao];
+             lista[posicao] = tmp;      
+        }
+    }
+    return lista;
+}
