7d9ba7ba3f7fedc0b10fb60cc579533f23029321
Ricardo <ric.asjunior@gmail.com>
2023-05-29 00:46:53 -0300

    refatorando

--- Receitas/receitas.js
+++ Receitas/receitas.js
@@ -1,34 +1,33 @@
-fetch("elementos.json").then((response) => {
-    response.json().then((cards) => {
-        return cards
+let blocos = []
+const pnlCatalogo = document.getElementById('pnlCatalogo')
+
+getJson()
+async function getJson(){
+    await fetch("elementos.json").then((response) => {
+        response.json().then((cards) => {
+            blocos = cards
+            exibirCards(blocos)
+        })
     })
-})
-getListaIngredientes(cards)
-
-function getListaIngredientes(receita){
-    forEach(receita , () => {
-        `<li>${elementoLista}</li>`
+}
+function exibirCards(listaDeCards){
+    listaDeCards.forEach(card => {
+        pnlCatalogo.innerHTML += `
+        <nav class="card p-3 m-4 card-largura">
+        <img src="${card.imagem}" class="card-img-top card-posicao-imagem"
+        alt="Imagem de macarrão com tomate">
+        <h2 class="text-center titulo-card p-2">${card.titulo}</h2>
+        <div class="card-body">
+            <ul class="border-bottom p-2">
+                <li>${card.ingredientes[0]}</li>
+                <li>${card.ingredientes[1]}</li>
+                <li>${card.ingredientes[2]}</li>
+                <li>${card.ingredientes[3]}</li>
+                <li>${card.ingredientes[4]}</li>
+            </ul>
+            <article class="card-text p-1">${card.preparo}</article>
+        </div>
+        </nav>
+        `
     })
 }
-
-function getCard(receita){
-
-}
-
-function preencheCatalogo(){
-
-}
-
-// onload ?????????
-`<nav class="card p-3 m-4 card-largura">
-    <img src="${imagem}" class="card-img-top card-posicao-imagem"
-    alt="Imagem de macarrão com tomate">
-    <h2 class="text-center titulo-card p-2">${titulo}</h2>
-    <div class="card-body">
-        <ul class="border-bottom p-2">
-            ${lista}
-        </ul>
-        <article class="card-text p-1">${preparo}</article>
-    </div>
-</nav>
-`
