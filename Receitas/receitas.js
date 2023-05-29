6a8f79cc0dff015b664fc73ae43529b50107bdfe
Ricardo <ric.asjunior@gmail.com>
2023-05-28 21:33:40 -0300

    Criando segunda parte do trabalho

--- Receitas/receitas.js
+++ Receitas/receitas.js
@@ -1,5 +1,12 @@
+fetch("elementos.json").then((response) => {
+    response.json().then((cards) => {
+        return cards
+    })
+})
+getListaIngredientes(cards)
+
 function getListaIngredientes(receita){
-    forEach(lista , () => {
+    forEach(receita , () => {
         `<li>${elementoLista}</li>`
     })
 }
@@ -13,3 +20,15 @@ function preencheCatalogo(){
 }
 
 // onload ?????????
+`<nav class="card p-3 m-4 card-largura">
+    <img src="${imagem}" class="card-img-top card-posicao-imagem"
+    alt="Imagem de macarrão com tomate">
+    <h2 class="text-center titulo-card p-2">${titulo}</h2>
+    <div class="card-body">
+        <ul class="border-bottom p-2">
+            ${lista}
+        </ul>
+        <article class="card-text p-1">${preparo}</article>
+    </div>
+</nav>
+`
